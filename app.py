23e2bc2f1f5874d639780382459de63149d01262
Kishor Naik <kishornaik191@gmail.com>
2026-07-25 01:13:16 +0530

    Fix app.run for Render

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ from routes.python import python
 
 from models import db
 import os
-app = Flask(__name__)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
 app.config["JWT_SECRET_KEY"] = "nktechvision_jwt_secret"
 jwt = JWTManager(app)
